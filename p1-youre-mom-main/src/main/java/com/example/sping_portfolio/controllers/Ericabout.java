14d5b7f6b140feba3cdda53d866c16da61a892bb
Charlie <trollol678534@gmail.com>
2021-12-09 20:43:27 -0800

    fixed some json added API

--- p1-youre-mom-main/src/main/java/com/example/sping_portfolio/controllers/Ericabout.java
+++ p1-youre-mom-main/src/main/java/com/example/sping_portfolio/controllers/Ericabout.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-public class Floraabout {
+public class Ericabout {
     @GetMapping("/floraabout")
     public String flora_about(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 
         model.addAttribute("name", name);
-        return "floraabout";
+        return "Ericabout";
     }
 }
 
